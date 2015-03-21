d5c614acd296202080fd4b72ec5a8f9517cfda27
Steven <stevenman42@gmail.com>
2015-03-21 11:18:38 -0400

    stuff

---
+++ bot.py
@@ -0,0 +1,9 @@
+
+
+class Bot:
+	"""The basic bot class"""
+
+	# Some basic variables used to configure the bot		
+	server = "irc.freenode.net" # Server
+	channel = "#sexywinners" # Channel
+	botnick = "dense_bot" # Your bots nick
